a99c92fb0d3a2abd297fd0d763bad9d0a7787ec4
Krystof Marasek <krystofmarasek@gmail.com>
2024-04-19 10:48:51 +0200

    Update RocketField.cs

--- ThrocketEscapeGame/Assets/Scripts/RocketField.cs
+++ ThrocketEscapeGame/Assets/Scripts/RocketField.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class RocketField : MonoBehaviour
 {
-    public void OnTriggerStay2D(Collider2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("ThrowItem"))
         {
             Debug.Log("ThowItem detected");
+            GameManager.instance.UpdateScoreAndPosition();
         }
     }
 }
